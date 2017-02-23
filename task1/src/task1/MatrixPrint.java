caa1249e3899dd54a6e2a42cf483b2d5c0dbdbba
StarkinDG <380506803080@yandex.ua>
2017-02-23 21:30:55 +0200

    sideSize add

--- task1/src/task1/MatrixPrint.java
+++ task1/src/task1/MatrixPrint.java
@@ -3,18 +3,19 @@ package task1;
 public class MatrixPrint {
 
 	public static void main(String[] args) {
+		int sideSize = 5;
 		int delta = 1;
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
+		for (int i = 0; i < sideSize; i++) {
+			for (int j = 0; j < sideSize; j++) {
 
-				if (j == i || j == (4 - i)) {
+				if (j == i || j == (sideSize -1 - i)) {
 					System.out.printf("%2s ", "*");
 				} else {
 					System.out.printf("%2d ", j + delta);
 				}
 
 			}
-			delta += 5;
+			delta += sideSize;
 			System.out.printf("\n");
 
 		}
