76389c6838951236ac87da5f06c042844969fb85
StarkinDG <380506803080@yandex.ua>
2017-02-25 15:09:05 +0200

    Bubble sort optimize

--- task1/src/task1/ArraySort.java
+++ task1/src/task1/ArraySort.java
@@ -3,15 +3,15 @@ package task1;
 public class ArraySort {
 
 	public static void main(String[] args) {
-		int[] array = { 23, 7, 10, 34, 1, 75, 4 };
+		int[] array = { 7, 53, 67, 80, 34 };
 		int length = array.length;
 
 		boolean flag;
 		int tmp = 0;
-
+		int bubbleCounter = 0;
 		do {
 			flag = false;
-			for (int i = 0, j = 1; j < length; i++, j++) {
+			for (int i = 0, j = 1; j < (length - bubbleCounter); i++, j++) {
 				if (array[i] > array[j]) {
 					tmp = array[i];
 					array[i] = array[j];
@@ -19,12 +19,12 @@ public class ArraySort {
 					flag = true;
 				}
 			}
+			bubbleCounter++;
 		} while (flag);
 
 		for (int i = 0; i < length; i++) {
 			System.out.print(array[i] + " ");
 
 		}
-
 	}
 }
