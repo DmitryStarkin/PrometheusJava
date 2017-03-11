5ce3489fb9f0e762efa0c51b138e7be6ce9c2332
StarkinDG <380506803080@yandex.ua>
2017-03-11 11:18:21 +0200

    week3 fin

---
+++ week4/src/task1/Comparator.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class Comparator {
+	public int compare(int a, int b){
+		if (a>b) {
+			return 1;
+		} else if (a == b){
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+}
