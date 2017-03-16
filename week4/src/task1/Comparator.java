8923cf54c3de3d79333a4072c7bce27063ef2f45
StarkinDG <380506803080@yandex.ua>
2017-03-16 19:29:56 +0200

    reformat

--- week4/src/task1/Comparator.java
+++ week4/src/task1/Comparator.java
@@ -1,10 +1,10 @@
 package task1;
 
 public class Comparator {
-	public int compare(int a, int b){
-		if (a>b) {
+	public int compare(int a, int b) {
+		if (a > b) {
 			return 1;
-		} else if (a == b){
+		} else if (a == b) {
 			return 0;
 		} else {
 			return -1;
